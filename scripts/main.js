a37b0356e89fbced226822ebc377e63f2fefad08
Santiago <santiago.aragon.99@gmail.com>
2022-04-15 02:46:35 -0300

    add footer nav & VueJS

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,67 @@
 Vue.createApp({
     data() {
-      return {
-          hideNav : true
-  
-      };
+        return {
+            hideNav: true,
+            allProducts: [],
+            productosFarmacia: [],
+            productosJuguetes: [],
+
+        };
     },
     created() {
-  
+        fetch("https://apipetshop.herokuapp.com/api/articulos")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.response)
+
+                this.allProducts = data.response
+                this.filtrarProductos()
+                console.log(this.productosFarmacia)
+                console.log(this.productosJuguetes)
+            })
+
     },
-  
+
     methods: {
-      toggleNavOpen(){
-         this.hideNav = false
-  
-      },
-      toggleNavClose(){
-        this.hideNav = true
-  
-     }
+        toggleNavOpen() {
+            this.hideNav = false
+
+        },
+        toggleNavClose() {
+            this.hideNav = true
+        },
+        filtrarProductos() {
+            this.productosFarmacia = this.allProducts.filter(producto => producto.tipo == "Medicamento")
+            this.productosJuguetes = this.allProducts.filter(producto => producto.tipo == "Juguete")
+        },
+
     },
-  }).mount("#app");
+}).mount("#app");
+
+if (document.URL.includes("contactenos")) {
+
+    const BTN1 = document.querySelector("#BTN1")
+
+    BTN1.addEventListener("click", function (event) {
+        event.preventDefault()
+
+        Swal.fire({
+            title: 'Gracias por enviarnos un mensaje! responderemos a la brevedad.',
+            width: 600,
+            icon: 'success',
+            imageUrl: '../assets/perrito.png',
+            imageHeight: 250,
+            color: '#7a3d8b',
+            background: '#fc5b5b',
+            backdrop: `
+          rgba(0,0,123,0.4)
+          url("../assets/pngegg.png")
+          left bottom
+          no-repeat
+        `,
+
+        })
+
+
+    });
+}
