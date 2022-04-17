0702eff78610676b387cf63b598a7a410f54e1d5
Santiago <santiago.aragon.99@gmail.com>
2022-04-17 05:23:12 -0300

    Fix cart & add functions

--- scripts/main.js
+++ scripts/main.js
@@ -21,8 +21,10 @@ Vue.createApp({
             idProductosEnCarrito: [],
             productosEnCarrito: [],
             productosEnStorage: [],
+            productoCarrito: {},
             precio: [],
             precioTotal: 0,
+            totalEnCarrito: 0,
 
             comprasEnStorage: [],
             cantidad: 0,
@@ -33,27 +35,33 @@ Vue.createApp({
             .then(response => response.json())
             .then(data => {
                 // console.log(data.response)
-
+                //guardo lo que me devuelve la api en una propiedad.
                 this.allProducts = data.response
+
                 this.carrouselActive = data.response.filter(producto => producto.tipo == "Juguete").slice(0, 4)
                 this.carrouselOne = data.response.filter(producto => producto.tipo == "Juguete").slice(4, 8)
                 this.carrouselPharmacyActive = data.response.filter(producto => producto.tipo == "Medicamento").slice(0, 4)
                 this.carrouselPharmacyOne = data.response.filter(producto => producto.tipo == "Medicamento").slice(4, 8)
+               
+                //filtro lo recibido para separar por tipo.
                 this.filtrarProductos()
                 this.filtrarPorPrecio()
 
+                //capturo el carrito guardado en storage
                 this.productosEnStorage = JSON.parse(localStorage.getItem("carrito"))
 
+                // si capturó algo, lo asigno en la propiedad productos en carrito y sumo su valor y cantidad
                 if (this.productosEnStorage) {
                     this.productosEnCarrito = this.productosEnStorage
                 }
                 this.PrecioCarrito();
+                this.totalEnCarrito = this.productosEnCarrito.map(prod => prod.cantidad).reduce((a, b) => a + b, 0)
+                // capturo las compras realizadas en local storage (en proceso)
                 this.productosComprados = JSON.parse(localStorage.getItem("compras"))
 
             })
 
     },
-
     methods: {
         toggleNavOpen() {
             this.hideNav = false
@@ -87,33 +95,72 @@ Vue.createApp({
         },
 
         añadirAlCarrito(producto) {
+            //
             this.idProductosEnCarrito = this.productosEnCarrito.map(producto => producto._id)
+            //PRODUCTO CARRITO ES UN NUEVO PRODUCTO DE TIPO OBJETO DENTRO DEL CARRITO
+            this.productoCarrito = this.productosEnCarrito.filter(prod => producto._id == prod._id)[0]
 
-            if (producto.stock > 0) {
-                this.productosEnCarrito.push(producto)
-               
-
-                localStorage.setItem("carrito", JSON.stringify(this.productosEnCarrito))
-
+            // SI YA EXISTE EL PRODUCTO, LE INCREMENTO A CANTIDAD (DENTRO DEL CARRITO)
+            if (this.productoCarrito != undefined) {
+                this.productoCarrito.cantidad++;
             }
+            // SI NO EXISTE EL PRODUCTO, INICIALIZO EL OBJETO CON LOS VALORES DEL PRODUCTO QUE VIENE DE PARAMETRO
+            else {
+                this.productoCarrito = {
+                    _id: producto._id,
+                    nombre: producto.nombre,
+                    precio: producto.precio,
+                    imagen: producto.imagen,
+                    cantidad: 1
+                };
+                // AGREGO EL OBJETO AL ARRAY DE PRODUCTOS
+                this.productosEnCarrito.push(this.productoCarrito);
+            }
+            // CALCULA EL TOTAL DE PRODUCTOS CON LA PROP CANTIDAD DENTRO DEL ARRAY DE PRODUCTOS 
+            this.totalEnCarrito = this.productosEnCarrito.map(prod => prod.cantidad).reduce((a, b) => a + b, 0)
 
+            // AGREGA EL NUEVO ARRAY PRODUCTOS EN CARRITO AL LOCAL STORAGE
+            localStorage.setItem("carrito", JSON.stringify(this.productosEnCarrito))
+            //NOTIFICACION DE AÑADIDO AL CARRITO
             Swal.fire('Añadido al carrito')
-            console.log(this.productosEnCarrito)
         },
+        //FUNCION QUE ELIMINA PRODUCTO DEL CARRITO
         eliminarDelCarrito(producto) {
-            this.productosEnCarrito = this.productosEnCarrito.filter(prod => prod._id != producto._id)
+            // SI EL OBJETO PRODUCTO TIENE CANTIDAD MAYOR A 1, SE DECREMENTA UNO.
+            if (producto.cantidad > 1) {
+                producto.cantidad--
+            }
+            // SINO, SE ELIMINA ESE OBJETO DEL ARRAY DE PRODUCTOS FILTRANDO LOS DISTINTOS AL SELECCIONADO
+            else {
+                this.productosEnCarrito = this.productosEnCarrito.filter(prod => prod._id != producto._id)
+            }
+            // ACA SE VUELVE A CALCULAR EL TOTAL DE PRODUCTOS QUE QUEDARON EN EL CARRITO
+            this.totalEnCarrito = this.productosEnCarrito.map(prod => prod.cantidad).reduce((a, b) => a + b, 0)
+
+
+            // SE ACTUALIZA EL LOCAL STORAGE CON EL ARRAY MODIFICADO SI FUESE EL CASO
             this.productosEnStorage = this.productosEnCarrito
             localStorage.setItem("carrito", JSON.stringify(this.productosEnStorage))
-            Swal.fire('Eliminado del Carrito')
 
+            //NOTIFICACION DE ELIMINADO DEL CARRITO
+            Swal.fire('Eliminado del Carrito')
         },
         comprarProductos() {
+            //RECORRE EL ARRAY DE PRODUCTOS EN EL CARRITO Y AGREGA AL ARRAY COMPRAS EN LOC STORAGE
             this.productosEnCarrito.forEach(producto => {
-                producto.stock -= 1
+                // producto.stock -= 1
                 this.comprasEnStorage.push(producto)
             })
-            localStorage.setItem("compras", JSON.stringify(this.comprasEnStorage))
+            // SE VACIA EL ARRAY DE PRODUCTOS EN CARRITO Y SE ACTUALIZA LA CANTIDAD DE ITEMS DENTRO DEL CARRITO
 
+            this.productosEnCarrito = []
+            this.totalEnCarrito = this.productosEnCarrito.map(prod => prod.cantidad).reduce((a, b) => a + b, 0)
+            // SE SOBRESCRIBE EL ARRAY DE LOS PRODUCTOS EN CARRITO EN LOCAL STORAGE
+
+            localStorage.setItem("carrito", JSON.stringify(this.productosEnCarrito))
+            // localStorage.setItem("compras", JSON.stringify(this.comprasEnStorage))
+
+            // NOTIFICACION POR REALIZAR COMPRA
             Swal.fire({
                 title: 'Muchas gracias por su compra!',
                 width: 600,
@@ -133,12 +180,19 @@ Vue.createApp({
         },
         PrecioCarrito() {
             if (this.productosEnCarrito.length > 0) {
-                this.precio = this.productosEnCarrito.map(producto=> producto.precio)
+                this.precio = this.productosEnCarrito.map(producto => producto.precio * producto.cantidad)
                 this.precioTotal = this.precio.reduce((a, b) => a + b, 0)
 
             } else {
                 this.precioTotal = 0
             }
+        },
+        vaciarCarrito() {
+            //VACIO EL ARRAY DE PRODUCTOS, CALCULO EL TOTAL EN CARRITO Y ACTUALIZO EL LOCAL STORAGE
+            this.productosEnCarrito = []
+            this.totalEnCarrito = this.productosEnCarrito.map(prod => prod.cantidad).reduce((a, b) => a + b, 0)
+            localStorage.setItem("carrito", JSON.stringify(this.productosEnCarrito))
+            Swal.fire('Se ha vaciado el carrito con exito')
         },
     },
     computed: {
@@ -146,8 +200,6 @@ Vue.createApp({
             this.filtrarPorPrecio()
             this.PrecioCarrito()
         },
-
-
     }
 }).mount("#app");
 
